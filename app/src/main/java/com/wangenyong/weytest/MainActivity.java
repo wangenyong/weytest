1c0792b22984e348c0d382aaa6042a92760cc9ba
wangenyong <wangenyong@gmail.com>
2015-07-10 09:22:53 +0800

    修改drawer header.

--- app/src/main/java/com/wangenyong/weytest/MainActivity.java
+++ app/src/main/java/com/wangenyong/weytest/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle = setupDrawerToggle();
 
         mDrawerLayout.setDrawerListener(drawerToggle);
+        mDrawerLayout.openDrawer(GravityCompat.START);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
