96f60582a07106b931675d62762617e83239fd0c
wangenyong <wangenyong@gmail.com>
2015-07-17 09:53:43 +0800

    添加线性图表数据。

--- app/src/main/java/com/wangenyong/weytest/activities/ViewsActivity.java
+++ app/src/main/java/com/wangenyong/weytest/activities/ViewsActivity.java
@@ -16,6 +16,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.wangenyong.mylibrary.tools.DimensionTools;
 import com.wangenyong.mylibrary.views.TintableImageView;
 import com.wangenyong.weytest.R;
@@ -179,6 +184,43 @@ public class ViewsActivity extends AppCompatActivity {
         backdropImg.setImageResource(R.drawable.img_view_chart_header);
         viewsCoolapsingLayout.setTitle(getString(R.string.view_chart));
         myViews.clear();
+
+        LineChart lineChart = new LineChart(this);
+        lineChart.setLayoutParams(lp);
+        myViews.add(new MyView(getString(R.string.chart_line), lineChart));
+
+        lineChart.setDescription(getString(R.string.chart_line));
+
+        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
+        ArrayList<Entry> valsComp2 = new ArrayList<Entry>();
+
+        Entry c1e1 = new Entry(100.000f, 0); // 0 == quarter 1
+        valsComp1.add(c1e1);
+        Entry c1e2 = new Entry(50.000f, 1); // 1 == quarter 2 ...
+        valsComp1.add(c1e2);
+        Entry c2e1 = new Entry(120.000f, 0); // 0 == quarter 1
+        valsComp2.add(c2e1);
+        Entry c2e2 = new Entry(110.000f, 1); // 1 == quarter 2 ...
+        valsComp2.add(c2e2);
+
+        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
+        setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        LineDataSet setComp2 = new LineDataSet(valsComp2, "Company 2");
+        setComp2.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(setComp1);
+        dataSets.add(setComp2);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.Q"); xVals.add("2.Q"); xVals.add("3.Q"); xVals.add("4.Q");
+
+        LineData data = new LineData(xVals, dataSets);
+
+        lineChart.setData(data);
+        lineChart.invalidate();
+
+
     }
 
     @Override
