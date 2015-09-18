32201e9df054f2484d68d6ce3699794532b3d999
wangenyong <wangenyong@gmail.com>
2015-09-18 10:36:48 +0800

    显示引导第一页内容。

--- app/src/main/java/com/wangenyong/weytest/activities/GuideActivity.java
+++ app/src/main/java/com/wangenyong/weytest/activities/GuideActivity.java
@@ -1,12 +1,21 @@
 package com.wangenyong.weytest.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.wangenyong.mylibrary.views.BlurringView;
 import com.wangenyong.weytest.R;
+import com.wangenyong.weytest.fragments.GuideFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -15,12 +24,49 @@ public class GuideActivity extends AppCompatActivity {
     @InjectView(R.id.blurredview_guideview) RelativeLayout blurredView;
     @InjectView(R.id.blurringView_guideview) BlurringView blurringView;
     @InjectView(R.id.viewpager_guideview) ViewPager viewPager;
+    @InjectView(R.id.tv_say_guideview) TextView sayTv;
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private GuideFragment fragment00, fragment01, fragment02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
         ButterKnife.inject(this);
+
+        fragment00 = GuideFragment.newInstance(getString(R.string.guideview_self_title), getString(R.string.guideview_self_content));
+        fragment01 = GuideFragment.newInstance(getString(R.string.guideview_like_title), getString(R.string.guideview_like_content));
+        fragment02 = GuideFragment.newInstance(getString(R.string.guideview_this_title), getString(R.string.guideview_this_content));
+
+        fragmentList.add(fragment00);
+        fragmentList.add(fragment01);
+        fragmentList.add(fragment02);
+
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/classicxing.ttf");
+        sayTv.setTypeface(typeFace);
+
+        //创建adapter
+        GuideAdapter adapter = new GuideAdapter(getSupportFragmentManager());
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(adapter);
+
     }
 
+    class GuideAdapter extends FragmentPagerAdapter {
+        public GuideAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+    }
 }
---
+++ app/src/main/java/com/wangenyong/weytest/fragments/GuideFragment.java
@@ -0,0 +1,79 @@
+package com.wangenyong.weytest.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wangenyong.weytest.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GuideFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GuideFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    @InjectView(R.id.tv_title) TextView title;
+    @InjectView(R.id.tv_content) TextView content;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GuideFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GuideFragment newInstance(String param1, String param2) {
+        GuideFragment fragment = new GuideFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public GuideFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_guide, container, false);
+        ButterKnife.inject(this, rootView);
+        title.setText(mParam1);
+        content.setText(mParam2);
+
+        return rootView;
+    }
+
+
+}
